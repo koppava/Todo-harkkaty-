369a1a47f9767d238aa64942de13b4f696d6bfe0
Ahti Ahde <ahti.ahde@loudrevolution.net>
2014-08-06 13:39:12 +0300

    Links the dummy data to the table

--- index.php
+++ index.php
@@ -1,41 +1,43 @@
 <?php
 
 $dummyData = [
-    [
-        "taskName" => "Todo harkka",
-        "responsible" => "koppava",
-        "deadline" => "in two days",
-        "done" => 0
-    ],
-    [
-        "taskName" => "Maailman valloitus",
-        "responsible" => "Suuri Johtaja",
-        "deadline" => "in two years",
-        "done" => 0
-    ],
-    [
-        "taskName" => "Kuussa käyminen",
-        "responsible" => "Neil Armstrong",
-        "deadline" => "35 years ago",
-        "done" => 1
-    ],
-    [
-        "taskName" => "Asiat joita lykkäsit huomiselle",
-        "responsible" => "Joku muu",
-        "deadline" => "yesterday",
-        "done" => 0
-    ],
-    [
-        "taskName" => "Sähkölaskun maksaminen",
-        "responsible" => "Kämppis",
-        "deadline" => "next week",
-        "done" => 1
-    ],
-    [
-        "taskName" => "Aurinkokunnan tuhoaminen",
-        "responsible" => "Aurinko",
-        "deadline" => "in ten billion years",
-        "done" => 0
+    "tasks" => [
+        [
+            "taskName" => "Todo harkka",
+            "assignee" => "koppava",
+            "deadline" => "in two days",
+            "done" => 0
+        ],
+        [
+            "taskName" => "Maailman valloitus",
+            "assignee" => "Suuri Johtaja",
+            "deadline" => "in two years",
+            "done" => 0
+        ],
+        [
+            "taskName" => "Kuussa käyminen",
+            "assignee" => "Neil Armstrong",
+            "deadline" => "35 years ago",
+            "done" => 1
+        ],
+        [
+            "taskName" => "Asiat joita lykkäsit huomiselle",
+            "assignee" => "Joku muu",
+            "deadline" => "yesterday",
+            "done" => 0
+        ],
+        [
+            "taskName" => "Sähkölaskun maksaminen",
+            "assignee" => "Kämppis",
+            "deadline" => "next week",
+            "done" => 1
+        ],
+        [
+            "taskName" => "Aurinkokunnan tuhoaminen",
+            "assignee" => "Aurinko",
+            "deadline" => "in ten billion years",
+            "done" => 0
+        ]
     ]
 ];
 
