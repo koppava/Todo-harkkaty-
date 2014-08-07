51cd21fbb6debdfb6063bb1dbf46001e91e74911
Ahti Ahde <ahti.ahde@loudrevolution.net>
2014-08-07 16:34:14 +0300

    Adds PUT method for the controller

--- source/Controller.php
+++ source/Controller.php
@@ -16,16 +16,28 @@ class Controller
     
     public function handleRequest($request)
     {
-        
         $response = array();
         if ($request['REQUEST_METHOD'] == 'GET') {
             $response = $this->service->getAll();
         }
         if ($request['REQUEST_METHOD'] == 'POST') {
             $this->service->persistRequestData($_POST);
-            $response = $this->service->getAll();
+        }
+        if ($request['REQUEST_METHOD'] == 'PUT') {
+            $this->service->saveData($this->parsePutData(file_get_contents("php://input")));
         }
         
         echo json_encode($response);
+    }
+    
+    private function parsePutData($streamData)
+    {
+        $dataArr = [];
+        $fields = explode('&', $streamData);
+        foreach ($fields as $field) {
+            $data = explode('=', $field);
+            $dataArr[$data[0]] = $data[1];
+        }
+        return $dataArr;
     }
 }
