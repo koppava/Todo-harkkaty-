2c501c349df4be5f98cc7737871632bf4229dcdf
Ahti Ahde <ahti.ahde@loudrevolution.net>
2014-08-07 16:33:40 +0300

    Adds data filtering and data saving for modifications

--- source/Service.php
+++ source/Service.php
@@ -20,15 +20,33 @@ class Service
         return ["tasks" => $this->dbAdapter->fetchAll()];
     }
     
-    public function persistRequestData($post)
+    public function persistRequestData($data)
     {
-        $this->dbAdapter->insertTodo([
-            'task_name' => $post['task_name'],
-            'assignee' => $post['assignee'],
-            'deadline' => DateTime::createFromFormat('d-m-Y', $post['deadline']),
-            'done' => 0
-        ]);
+        $data = $this->filterData($data);
         
-        return array_pop($this->getAll());
+        $this->dbAdapter->insertTask($data);
+    }
+    
+    public function saveData($data)
+    {
+        $data = $this->filterData($data);
+        
+        $id = $data['id'];
+        unset($data['id']);
+        $this->dbAdapter->updateTask($id, $data);
+    }
+    
+    private function filterData($data)
+    {
+        foreach ($data as $key => $value) {
+            if ($key == 'id' || $key == 'done') {
+                $data[$key] = (int) $value;
+            }
+            if ($key == 'deadline') {
+                $data[$key] = DateTime::createFromFormat('d-m-Y', $value)->format('Y-m-d');
+            }
+        }
+        
+        return $data;
     }
 }
