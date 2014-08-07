0ee17a563e8f429db11a51c9aecd008d90846676
Ahti Ahde <ahti.ahde@loudrevolution.net>
2014-08-07 16:25:30 +0300

    Adds updateTask for DbAdapter and refactors the structure for better reusability

--- source/DbAdapter.php
+++ source/DbAdapter.php
@@ -16,15 +16,76 @@ class DbAdapter extends PDO
         parent::__construct($dsn, $username, $password, $options);
     }
     
-    public function insertTodo($data)
+    private function prepareInsertSql($data)
     {
-        $statement = $this->prepare('INSERT INTO task (task_name, assignee, deadline, done)'
-                . ' VALUES (:task_name, :assignee, :deadline, :done);');
-        $statement->bindParam(':task_name', $data['task_name']);
-        $statement->bindParam(':assignee', $data['assignee']);
-        $statement->bindParam(':deadline', $data['deadline']->format('Y-m-d'));
-        $statement->bindParam(':done', $data['done']);
+        $sql = 'INSERT INTO task (';
+        
+        $columns = array_keys($data);
+        foreach ($columns as $column) {
+            if ($columns[0] != $column) {
+                $sql .= ',';
+            }
+            $sql .= $column;
+        }
+        
+        $sql .= ') VALUES (';
+        
+        foreach ($columns as $column) {
+            if ($columns[0] != $column) {
+                $sql .= ',';
+            }
+            $sql .= ':' . $column;
+        }
+        
+        $sql .= ');';
+        
+        return $sql;
+    }
+    
+    private function prepareUpdateSql($data)
+    {
+        $sql = 'UPDATE task SET ';
+        
+        $columns = array_keys($data);
+        foreach ($columns as $column) {
+            if ($columns[0] != $column) {
+                $sql .= ',';
+            }
+            $sql .= $column .'=:'. $column;
+        }
+        
+        $sql .= ' WHERE id=:id;';
+        return $sql;
+    }
+    
+    private function bindData($statement, $data)
+    {
+        foreach (array_keys($data) as $column) {
+            // Bind params takes reference to var, do we must use the array
+            $statement->bindParam(':' . $column, $data[$column]);
+        }
+        
+        return $statement;
+    }
+    
+    public function insertTask($data)
+    {
+        $statement = $this->prepare($this->prepareInsertSql($data));
+        
+        $this->bindData($statement, $data);
+        
         $statement->execute();
+    }
+    
+    public function updateTask($id, $data)
+    {
+        $statement = $this->prepare($this->prepareUpdateSql($data));
+        $statement->bindParam(':id', $id);
+        $this->bindData($statement, $data);
+        
+        if ($statement->execute() === false) {
+            var_dump($statement->errorInfo());
+        }
     }
     
     public function fetchAll()
